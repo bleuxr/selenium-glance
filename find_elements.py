ed87c816adb0d75d25f0e0dbe103ff4563c93fd4
bleuxr <38353817+bleuxr@users.noreply.github.com>
2018-07-20 17:11:01 +0800

    add find_elements.py

---
+++ find_elements.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+import time
+import os
+
+dr=webdriver.Chrome()
+file_path='file:///'+os.path.abspath('checkbox.html')
+dr.get(file_path)
+
+checkboxes = dr.find_elements_by_css_selector('input[type=checkbox]')
+for checkbox in checkboxes:
+    checkbox.click()
+time.sleep(1)
+dr.refresh()
+time.sleep(2)
+
+print(len(dr.find_elements_by_css_selector('input[type=checkbox]')))
+
+inputs=dr.find_elements_by_tag_name('input')
+for input in inputs:
+    if input.get_attribute('type')=='checkbox':
+        input.click()
+
+time.sleep(1)
+
+dr.find_elements_by_tag_name('input')
+for input in inputs:
+    if input.get_attribute('type')=='checkbox':
+        input.click()
+
+time.sleep(1)
+
+dr.find_elements_by_css_selector('input[type=checkbox]').pop().click()
+time.sleep(1)
+
+dr.quit()
